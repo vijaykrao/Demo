cd4dd6d9400fea57b2326c9284bca3e53a738bbb
vijaykrao <vijayraots@yahoo.com>
2018-05-16 11:41:52 +0530

    app final commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+
+<App  baseUrl={'http://localhost:8153/api.rsc'} user={'admin'} pass={'8e3E4b4g4D4x7s5M7b8p'} />, 
+ document.getElementById('root'));
+registerServiceWorker();
